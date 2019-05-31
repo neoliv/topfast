4905329e1dcc3d6014b806a80d0b5872b06ea727
Olivier Arsac <oliv@arsac.org>
2019-05-31 15:32:13 +0200

    Faster forks of subcommands.

--- hog.sh
+++ hog.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Mimic a poorly coded shell script that forks a lot of commands.
+# Mimic a poorly coded shell script that forks a lot of short lived commands.
 
 while true; do
-    find $GOPATH -iname "*.go" -print0 | xargs -0 -n 1 awk '{print $1}' | grep ".*" | wc -c
+    # Fork a awk+sed+tr+grep+wc for each .go file in $GOPATH
+    find $GOPATH -iname "*.go" -print0 |  while read -r -d $'\0' file ; do echo "$file" | awk '{print}' | sed 's/a/A/g'| tr '[A-Z]' '[a-z]' | egrep ".*" | wc -c; done
 done
